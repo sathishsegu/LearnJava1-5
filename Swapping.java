56cee8c6a15f208b90a5b8a50318d726602ff13c
Sathish <Sathishsegu123@gmail.com>
2023-03-06 16:46:35 +0530

    A new file Swapping is added to this repo

---
+++ Swapping.java
@@ -0,0 +1,10 @@
+public class Swapping {
+    public static void main(String args[]){
+        int a = 10,b= 20;
+        int c = a;
+        a = b;
+        b = c;
+        System.out.println("a is: "+a);
+        System.out.println("b is: "+b);
+    }
+}
