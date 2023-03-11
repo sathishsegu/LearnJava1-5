4d53866d3eeb03d7079131aaaa8a80318a7a35f2
Sathish <Sathishsegu123@gmail.com>
2023-03-11 09:23:23 +0530

    New file fibonacci has added

---
+++ fibonacci.java
@@ -0,0 +1,13 @@
+public class fibonacci {
+    public static void main(String args[]){
+        int i,n1=0,n2=1,n3;
+        System.out.println(n1);
+        System.out.println(n2);
+        for(i=1;i<=10;i++){
+            n3 = n1+n2;
+            System.out.println(n3);
+            n1 = n2;
+            n2 = n3;
+        }
+        }
+    }
